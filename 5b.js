6b9d60ccd848be13e799b324c6a15f6c5ccafce7
Daryl Haresign <dharesign@bloomberg.net>
2020-12-07 23:40:25 +0000

    Day 5

---
+++ 5b.js
@@ -0,0 +1,27 @@
+const data = require("./5.json");
+
+function decode(qr) {
+    let row = 0;
+    if (qr[0] === "B") row += 64;
+    if (qr[1] === "B") row += 32;
+    if (qr[2] === "B") row += 16;
+    if (qr[3] === "B") row +=  8;
+    if (qr[4] === "B") row +=  4;
+    if (qr[5] === "B") row +=  2;
+    if (qr[6] === "B") row +=  1;
+
+    let col = 0;
+    if (qr[7] === "R") col += 4;
+    if (qr[8] === "R") col += 2;
+    if (qr[9] === "R") col += 1;
+
+    return row * 8 + col;
+}
+
+let ids = data.map(decode);
+
+for (let i = 0; i < 933; ++i) {
+    if (ids.indexOf(i) === -1) {
+        console.log(`Missing ${i}`);
+    }
+}
